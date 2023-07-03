ad1e8337289deb4f47646a24ffbfcb45fc1611aa
Ryan Kang <ryan.sanhae.kang@gmail.com>
2023-07-02 20:40:51 -0600

    created all functions for viewing, adding and updating employee

--- utils/db.js
+++ utils/db.js
@@ -1,9 +1,9 @@
 // Import and require inquirer
 const inquirer = require('inquirer');
-const db= require ('../config/connection');
+const db = require('../config/connection');
 
 // Starting screen once logged into node
-let employee_db= function() {
+let employee_db = function () {
     inquirer.prompt({
         type: 'list',
         name: 'option',
@@ -18,11 +18,11 @@ let employee_db= function() {
             "Update Employee Role",
             "Quit Application",
         ]
-    }).then (function(result){
-        console.log('You selected: '+ result.option);
+    }).then(function (result) {
+        console.log('You selected: ' + result.option);
 
         // Switch statements for all choices in prompt
-        switch (result.option){
+        switch (result.option) {
             case "View all department":
                 viewDepartment();
                 break;
@@ -51,26 +51,133 @@ let employee_db= function() {
 };
 
 // viewDepartment() Function
-function viewDepartment(){
-    db.query('SELECT * FROM department', function (err,res) {
-        if(err) throw (err);
+function viewDepartment() {
+    db.query('SELECT * FROM department', function (err, res) {
+        if (err) throw (err);
         console.table(res);
         employee_db();
     });
-}
+};
 
 // viewRoles() Function
 function viewRoles() {
-    db.query('SELECT * FROM role', function (err,res){
-        if(err) throw (err);
+    db.query('SELECT * FROM role', function (err, res) {
+        if (err) throw (err);
         console.table(res);
         employee_db();
     })
+};
+
+// viewEmployees() Function
+function viewEmployees() {
+    db.query('SELECT * FROM employee', function (err, res) {
+        if (err) throw (err);
+        console.table(res);
+        employee_db();
+    })
+};
+
+// addDepartment() Function
+function addDepartment() {
+    inquirer.prompt({
+        type: "input",
+        message: "What is the department called?",
+        name: "departmentName"
+    }).then(function (answer) {
+        db.query("INSERT INTO department (name) VALUES (?)", [answer.departmentName], function (err, res) {
+            if (err) throw (err);
+            console.table(res);
+            employee_db();
+        })
+    })
+};
+
+// addRole() Function
+function addRole() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Please input role",
+            name: "roleTitle"
+        },
+        {
+            type: "input",
+            message: "Please input salary for this role",
+            name: "salary"
+        },
+        {
+            type: "input",
+            message: "Please input department ID number",
+            name: "departmentID"
+        }
+    ]).then(function (answer) {
+        db.query("INSTERT INTO role (title,salary,departmentID) VALUES (?,?,?)", [answer.roleTitle, answer.salary, answer.departmentID], function (err, res) {
+            if (err) throw (err);
+            console.table(res);
+            employee_db()
+        })
+    })
+};
+
+// addEmployee() Function
+function addEmployee() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "First name of Employee?",
+            name: "firstName"
+        },
+        {
+            type: "input",
+            message: "Last name of Employee?",
+            name: "lastName"
+        },
+        {
+            type: "input",
+            message: "Please input employee's role ID number",
+            name: "roleID"
+        },
+        {
+            type: "input",
+            message: "Please input the manager ID number",
+            name: "managerID"
+        }
+    ]).then(function (answer) {
+        db.query('INSERT INTO employee (firstName, lastName, roleID, managerID) VALUES(?,?,?,?)', [answer.firstName, answer.lastName, answer.roleID, answer.managerID], function (err, res) {
+            if (err) throw (err);
+            console.table(res);
+            employee_db();
+        });
+    });
+};
+
+// updateEmployee() Function
+function updateEmployee(){
+    inquirer.prompt([
+        {
+            type:"input",
+            message:"Which employee would you like to update?",
+            name:"employeeUpdate"
+        },
+        {
+            type:"input",
+            message:"What do you want to change the new Role to?",
+            name:"updateRole"
+        }
+        
+    ]).then(function(answer){
+       db.query('UPDATE employee SET roleID=? WHERE firstName= ?', [answer.updateRole, answer.employeeUpdate], function(err,res){
+        if(err) throw(err);
+        console.table(res);
+        employee_db();
+       })
+    })
+};
+
+// quitApplication() Function
+function quitApplication(){
+    Connection.end();
+    process.end();
 }
 
-
-
-
-
-
-module.exports= {employee_db};
+module.exports = { employee_db };
